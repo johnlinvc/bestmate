163e00a9a26fc2716e8492e6ae9bc117f57f4f17
johnlinvc <johnlinvc@gmail.com>
2016-07-12 00:41:48 +0800

    able to find clicked block

--- bestmate/GameScene.swift
+++ bestmate/GameScene.swift
@@ -9,21 +9,22 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    var blocks:[SKLabelNode]? = nil
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
         let names = (1...25).map {
             return String($0)
         }
-        let blocks = names.map {
+        blocks = names.map {
             return SKLabelNode(text: $0)
         }
         let width:CGFloat = 90
-        for b in blocks {
+        for b in blocks! {
             b.fontName = "helvetica"
             b.fontSize = width/2
             b.fontColor = UIColor.blackColor()
         }
-        for (idx,b) in blocks.enumerate() {
+        for (idx,b) in blocks!.enumerate() {
             let xIdx = (idx % 5) - 2
             let yIdx = 2 - (idx / 5)
             let x:CGFloat = CGRectGetMidX(self.frame) + width * CGFloat(xIdx)
@@ -35,22 +36,14 @@ class GameScene: SKScene {
     
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
         
         for touch in touches {
             let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
+            let idx = blocks?.indexOf{
+                return $0.containsPoint(location)
+            }
+            guard let iidx = idx else { continue }
+            let block = blocks?[iidx]
         }
     }
    
